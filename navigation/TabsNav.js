3b7259f529bfc916673cf0ca3d0e79055c486646
elir-mahad <mmahad280@gmail.com>
2022-01-21 16:29:14 -0500

    Add bottom tab navigator and jsx to TabsNav

--- navigation/TabsNav.js
+++ navigation/TabsNav.js
@@ -4,11 +4,14 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Tab1 from "../components/Tab1";
 import Tab2 from "../components/Tab2";
 
+const Tab = createBottomTabNavigator();
+
 const TabsNav = () => {
   return (
-    <View>
-      <Text>Tabs Nav</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Tab 1" component={Tab1} />
+      <Tab.Screen name="Tab 2" component={Tab2} />
+    </Tab.Navigator>
   );
 };
 

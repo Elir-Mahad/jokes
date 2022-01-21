1ff1ba0cebfe3367c7ab9c1c7e35a77ccdfcb5cc
elir-mahad <mmahad280@gmail.com>
2022-01-21 16:28:04 -0500

    Add imports to TabsNav

--- navigation/TabsNav.js
+++ navigation/TabsNav.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Tab1 from "../components/Tab1";
+import Tab2 from "../components/Tab2";
 
 const TabsNav = () => {
   return (

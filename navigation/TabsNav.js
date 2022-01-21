563dd7f0fd46ea6eae3f1a84d5ff40af5e7ff65b
elir-mahad <mmahad280@gmail.com>
2022-01-21 16:27:16 -0500

    Create navigation folder and TabsNav component

---
+++ navigation/TabsNav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const TabsNav = () => {
+  return (
+    <View>
+      <Text>Tabs Nav</Text>
+    </View>
+  );
+};
+
+export default TabsNav;

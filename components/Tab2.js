26b8963406c10cc55f16207da2aeab89058f5117
elir-mahad <mmahad280@gmail.com>
2022-01-21 16:25:59 -0500

    Add jsx and styles to Tab2

--- components/Tab2.js
+++ components/Tab2.js
@@ -4,10 +4,18 @@ import { StyleSheet, Text, View } from "react-native";
 
 const Tab2 = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Tab 2</Text>
     </View>
   );
 };
 
 export default Tab2;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});

e5aa318974e424248587ccaac831bdddb5dec12e
elir-mahad <mmahad280@gmail.com>
2022-01-21 20:30:42 -0500

    In components folder, create DrawerButton component

---
+++ components/DrawerButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const DrawerButton = () => {
+  return (
+    <View>
+      <Text> Drawer button</Text>
+    </View>
+  );
+};
+
+export default DrawerButton;

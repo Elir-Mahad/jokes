15639a428142160181bcae764dc3b068c6a804f5
elir-mahad <mmahad280@gmail.com>
2022-01-21 20:33:57 -0500

    In navigation folder, create DrawerNav component

---
+++ navigation/DrawerNav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const DrawerNav = () => {
+  return (
+    <View>
+      <Text>Drawer nav</Text>
+    </View>
+  );
+};
+
+export default DrawerNav;

7b67486040b9d48b62734e125e1f024fc93ebf0f
elir-mahad <mmahad280@gmail.com>
2022-01-24 11:14:11 -0500

    In FullJoke, astore route.params in constants

--- components/FullJoke.js
+++ components/FullJoke.js
@@ -2,6 +2,8 @@ import React from "react";
 import { View, Text } from "react-native";
 
 const FullJoke = ({ route }) => {
+  const setUpText = route.params.set;
+  const punchLineText = route.params.del;
   return (
     <View>
       <Text>Full joke</Text>

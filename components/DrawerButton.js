96f5b1e42479957b8d10deb7b3085b00ec0ac47a
elir-mahad <mmahad280@gmail.com>
2022-01-24 15:07:58 -0500

    Add comments to DrawerButton

--- components/DrawerButton.js
+++ components/DrawerButton.js
@@ -1,18 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Button, Platform } from "react-native";
-//
 import {
   DrawerContentScrollView,
   DrawerItemList,
 } from "@react-navigation/drawer";
-//
 import Axios from "axios";
-//
 import Constants from "expo-constants";
 import * as Notifications from "expo-notifications";
-//
 import { useNavigation } from "@react-navigation/native";
-//
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
@@ -26,7 +21,7 @@ const DrawerButton = (props) => {
 
   const [jokeSetup, setJokeSetup] = useState("");
   const [jokeDelivery, setJokeDelivery] = useState("");
-  //
+  //  initialize the joke api useState hooks
   const getJoke = () => {
     Axios.get("https://v2.jokeapi.dev/joke/any?type=twopart").then(
       (response) => {
@@ -35,12 +30,12 @@ const DrawerButton = (props) => {
       }
     );
   };
-  //
+  // create the getJoke function to fetch api data
   const [expoPushToken, setExpoPushToken] = useState("");
   const [notification, setNotification] = useState(false);
   const notificationListener = useRef();
   const responseListener = useRef();
-  //
+  // initialize the expo notification’s useState hooks
   useEffect(() => {
     registerForPushNotificationsAsync().then((token) =>
       setExpoPushToken(token)
@@ -63,7 +58,7 @@ const DrawerButton = (props) => {
       Notifications.removeNotificationSubscription(responseListener.current);
     };
   }, []);
-  //
+  // add the expo notification’s useEffect hook
   async function schedulePushNotification(jokeSetup, jokeDelivery) {
     await Notifications.scheduleNotificationAsync({
       content: {
@@ -74,22 +69,26 @@ const DrawerButton = (props) => {
       trigger: { seconds: 2 },
     });
   }
-  //
+  // add the expo notification’s schedulePushNotification function
+  // pass the api useState hooks into it
   const sendPush = () => {
     schedulePushNotification(jokeSetup, jokeDelivery);
   };
-  //
+  // create the sendPush function to send push notifications
+  // insert the schedulePushNotification function in it
   const getJokeSendPush = () => {
     getJoke();
     sendPush();
   };
+  // create the getJokeSendPush function
+  // insert the getJoke and sendPush functions in it
 
   //!------------------- User clicks push notification, gets redirected to full joke page
 
   const navigation = useNavigation();
   //
   const lastNotificationResponse = Notifications.useLastNotificationResponse();
-  //
+  // add the lastNotificationResponse hook, to manage the push notification click event
   useEffect(() => {
     if (lastNotificationResponse) {
       navigation.navigate("Full Joke", {
@@ -98,7 +97,7 @@ const DrawerButton = (props) => {
       });
     }
   }, [navigation, lastNotificationResponse]);
-  //
+  // add a useEffect hook, so on notification tab, user is pushed to the FullJoke screen
   return (
     <DrawerContentScrollView {...props}>
       <DrawerItemList {...props} />
@@ -109,7 +108,7 @@ const DrawerButton = (props) => {
 
 export default DrawerButton;
 
-//---  Expo push notification code from docs
+// add the expo notification’s registerForPushNotificationsAsync function
 
 async function registerForPushNotificationsAsync() {
   let token;

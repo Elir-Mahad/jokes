4c0c82d5b16482a83c44520ad72843c2eff04a86
elir-mahad <mmahad280@gmail.com>
2022-01-24 11:09:30 -0500

    In FullJoke, add props

--- components/FullJoke.js
+++ components/FullJoke.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-const FullJoke = () => {
+const FullJoke = ({ route }) => {
   return (
     <View>
       <Text>Full joke</Text>
